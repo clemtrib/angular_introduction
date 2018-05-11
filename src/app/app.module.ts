d5f459c7749e494ae6b27191b96f294f237d6e9e
tribouillard <clement.tribouillard@econocom.com>
2018-05-11 10:40:09 +0200

    architecture project test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NouveauComponent } from './nouveau/nouveau.component';
+import { HeaderComponent } from './header/header.component';
+import { CocktailsListComponent } from './cocktails-list/cocktails-list.component';
+import { CocktailsDetailsComponent } from './cocktails-details/cocktails-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NouveauComponent
+    /* NouveauComponent, */
+    HeaderComponent,
+    CocktailsListComponent,
+    CocktailsDetailsComponent
   ],
   imports: [
     BrowserModule,
